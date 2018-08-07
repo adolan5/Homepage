c4d5b36fff35282a53d874da01671474c10d5ed9
adolan5 <andy.dolan.dev@gmail.com>
2018-08-06 22:59:25 -0600

    Stub index and app

---
+++ src/app.js
@@ -0,0 +1,19 @@
+/**
+ * Main homepage application container.
+ */
+
+'use strict';
+
+const React = require('react');
+const Component = React.Component;
+
+class App extends Component {
+    // Main container render function
+    render() {
+        return (
+            <h1>Main container</h1>
+        );
+    }
+}
+
+module.exports = App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 /**
  * Main homepage entry.
+ * Kicks off initial rendering of our application's React components.
  */
 
+'use strict';
+
+// Bring in React dependencies
+const React = require('react');
+const ReactDOM = require('react-dom');
+// Bring in main app container component (top level component)
+const App = require('./app');
+
+// Simply call the DOM render to start things off.
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
