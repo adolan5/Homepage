4edb992d5d2c7c61a781c4057ef0f109b6dbb0ab
adolan5 <andy.dolan.dev@gmail.com>
2018-08-13 16:02:31 -0600

    Stub sidebar component

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const Component = React.Component;
 
 // Import components
 const Header = require('./components/header.js');
+const Sidebar = require('./components/sidebar.js');
 
 // Import global style
 require('./style/app.less');
@@ -20,6 +21,7 @@ class App extends Component {
         return (
             <div>
                 <Header />
+                <Sidebar />
                 <div className='logo'>
                     <img src='/images/CSULogo.png' />
                 </div>
---
+++ src/components/sidebar.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const React = require('react');
+const Component = React.Component;
+
+/**
+ * Sidebar component.
+ */
+class Sidebar extends Component {
+    render() {
+        return null;
+    }
+}
+
+module.exports = Sidebar;
