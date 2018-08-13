884b6462da8a71fc57bb18331a0ddcf0aab1cd54
adolan5 <andy.dolan.dev@gmail.com>
2018-08-12 22:08:18 -0600

    Stub main container style

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
-/**
- * Main homepage application container.
- */
-
 'use strict';
 
 const React = require('react');
 const Component = React.Component;
 
+// Import style
+require('./style/app.less');
+
+/**
+ * Main homepage application container.
+ */
 class App extends Component {
     // Main container render function
     render() {
         return (
-            <h1>Main container</h1>
+            <div className='main'>
+                <h1>Main Container</h1>
+            </div>
         );
     }
 }
