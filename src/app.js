b810e81547f69dcca61d6a09b6170dab7d89ee8d
adolan5 <andy.dolan.dev@gmail.com>
2018-08-14 09:04:04 -0600

    Styling fixes for more dynamic header

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ class App extends Component {
      */
     render() {
         return (
-            <div>
+            <div className='app'>
                 <Header />
                 <Sidebar />
                 <div className='logo'>
