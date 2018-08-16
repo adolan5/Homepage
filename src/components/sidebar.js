ed01c0d58c6e4535583d040eaf52d498320630dd
adolan5 <andy.dolan.dev@gmail.com>
2018-08-15 22:02:41 -0600

    Stub sidebar style

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,12 +3,22 @@
 const React = require('react');
 const Component = React.Component;
 
+require('../style/components/sidebar.less');
+
 /**
  * Sidebar component.
  */
 class Sidebar extends Component {
     render() {
-        return null;
+        return (
+            <div className='sidebar'>
+            </div>
+        );
+    }
+
+    doThing() {
+        console.log('Did thing');
+        console.log(document.getElementById('logo').classList[0].toggle('collapsed'));
     }
 }
 
