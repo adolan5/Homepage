1b75027c1349259db596317f5945dafc3e193ded
adolan5 <andy.dolan.dev@gmail.com>
2018-08-18 02:47:55 -0600

    Assume that text is input as an array in a JSON file

--- src/components/contentcontainer.js
+++ src/components/contentcontainer.js
@@ -19,9 +19,22 @@ class ContentContainer extends Component {
         return (
             <div className='contentcontainer'>
                 <h1><i>{ this.props.data.header }</i></h1>
-                <p>{ this.props.data.text }</p>
+                { this.getText() }
             </div>
         );
+    }
+
+    /**
+     * Utility function to extract text snippets from data and render
+     * them on their own lines.
+     */
+    getText() {
+        // Take the array of text snippets and transform them into <p>'s
+        return this.props.data.text.map((snippet, i) => {
+            // Since we're returning an array of JSX elements, we should give them a key;
+            // let's have that be i, the index of the snippet
+            return <p key={ i }>{ snippet }</p>
+        });
     }
 }
 
